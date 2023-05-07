f865acbd6a4ea3fd8c26c5cfe7f79a6f433fb2e0
Bikrant Jung <bikrantjung1214@gmail.com>
2023-05-07 11:45:25 +0545

    Initial Setup Next/13

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="border">A</div>
+    </div>
+  );
+}
+
+export default LoginPage;
